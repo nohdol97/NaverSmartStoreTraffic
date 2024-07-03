ecfe81a77836211628a3d24b9990515a844535ad
nohdol97 <nohdol97@gmail.com>
2024-07-03 14:50:30 +0900

    create_driver 문제 발생시 해결

--- driverInfo.py
+++ driverInfo.py
@@ -14,35 +14,38 @@ def get_new_cache_number(previous_number):
     return random.choice(numbers)
 
 def create_driver(profileNum, ID = None):
-    # 프로필 디렉토리 경로 지정
-    global previous_number
-    cache_number = get_new_cache_number(previous_number)
-    original_profile_dir = os.path.join(os.getcwd(), f"chrome_profile{cache_number}")
-    temp_profile_dir = os.path.join(os.getcwd(), f"temp_chrome_profile{profileNum}")
+    try:
+        # 프로필 디렉토리 경로 지정
+        global previous_number
+        cache_number = get_new_cache_number(previous_number)
+        original_profile_dir = os.path.join(os.getcwd(), f"chrome_profile{cache_number}")
+        temp_profile_dir = os.path.join(os.getcwd(), f"temp_chrome_profile{profileNum}")
 
-    # 기존 임시 프로필 디렉토리가 있다면 삭제
-    if os.path.exists(temp_profile_dir):
-        shutil.rmtree(temp_profile_dir)
+        # 기존 임시 프로필 디렉토리가 있다면 삭제
+        if os.path.exists(temp_profile_dir):
+            shutil.rmtree(temp_profile_dir)
 
-    # 원본 프로필 디렉토리를 임시 디렉토리로 복사
-    shutil.copytree(original_profile_dir, temp_profile_dir)
+        # 원본 프로필 디렉토리를 임시 디렉토리로 복사
+        shutil.copytree(original_profile_dir, temp_profile_dir)
 
-    # Chrome option 설정
-    chrome_options = webdriver.ChromeOptions()
-    chromeOptions.addOptimization(chrome_options, temp_profile_dir)
-    chromeOptions.addMobile(chrome_options)
-    hiPaiProxy = chromeOptions.addProxy(chrome_options, ID)
+        # Chrome option 설정
+        chrome_options = webdriver.ChromeOptions()
+        chromeOptions.addOptimization(chrome_options, temp_profile_dir)
+        chromeOptions.addMobile(chrome_options)
+        hiPaiProxy = chromeOptions.addProxy(chrome_options, ID)
 
-    # Xvfb 설정
-    os.environ['DISPLAY'] = ':99'
+        # Xvfb 설정
+        os.environ['DISPLAY'] = ':99'
 
-    # ChromeDriver를 webdriver_manager를 사용하여 자동으로 설치 및 설정
-    service = Service(ChromeDriverManager().install())
+        # ChromeDriver를 webdriver_manager를 사용하여 자동으로 설치 및 설정
+        service = Service(ChromeDriverManager().install())
 
-    # WebDriver 인스턴스 생성    
-    driver = webdriver.Chrome(service=service, options=chrome_options)
+        # WebDriver 인스턴스 생성    
+        driver = webdriver.Chrome(service=service, options=chrome_options)
 
-    # Chrome 최적화
-    # chromeOptions.intercept(driver)
+        # Chrome 최적화
+        # chromeOptions.intercept(driver)
 
-    return driver, temp_profile_dir, hiPaiProxy
+        return driver, temp_profile_dir, hiPaiProxy
+    except:
+        create_driver(profileNum, ID)
