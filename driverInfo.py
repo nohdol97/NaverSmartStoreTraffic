4aeed8573dc1e7ebe16404bbac74d5b66a973571
nohdol97 <nohdol97@gmail.com>
2024-07-03 16:33:58 +0900

    driverInfo, hiPaiProxy 에러 핸들링 수정

--- driverInfo.py
+++ driverInfo.py
@@ -2,51 +2,49 @@ from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from seleniumwire import webdriver
-import os, random, shutil
+import os, time, shutil
 import chromeOptions
 
-previous_number = None
+previous_number = 0
 
 def get_new_cache_number(previous_number):
-    numbers = list(range(1, 11))  # 1부터 10까지의 숫자를 생성
-    if previous_number in numbers:
-        numbers.remove(previous_number)
-    return random.choice(numbers)
+    return (previous_number + 3) % 10
 
 def create_driver(profileNum, ID = None):
-    try:
-        # 프로필 디렉토리 경로 지정
-        global previous_number
-        cache_number = get_new_cache_number(previous_number)
-        original_profile_dir = os.path.join(os.getcwd(), f"chrome_profile{cache_number}")
-        temp_profile_dir = os.path.join(os.getcwd(), f"temp_chrome_profile{profileNum}")
+    for i in range(10):
+        try:
+            # 프로필 디렉토리 경로 지정
+            global previous_number
+            cache_number = get_new_cache_number(previous_number)
+            original_profile_dir = os.path.join(os.getcwd(), f"chrome_profile{cache_number}")
+            temp_profile_dir = os.path.join(os.getcwd(), f"temp_chrome_profile{profileNum}")
 
-        # 기존 임시 프로필 디렉토리가 있다면 삭제
-        if os.path.exists(temp_profile_dir):
-            shutil.rmtree(temp_profile_dir)
+            # 기존 임시 프로필 디렉토리가 있다면 삭제
+            if os.path.exists(temp_profile_dir):
+                shutil.rmtree(temp_profile_dir)
 
-        # 원본 프로필 디렉토리를 임시 디렉토리로 복사
-        shutil.copytree(original_profile_dir, temp_profile_dir)
+            # 원본 프로필 디렉토리를 임시 디렉토리로 복사
+            shutil.copytree(original_profile_dir, temp_profile_dir)
 
-        # Chrome option 설정
-        chrome_options = webdriver.ChromeOptions()
-        chromeOptions.addOptimization(chrome_options, temp_profile_dir)
-        chromeOptions.addMobile(chrome_options)
-        hiPaiProxy = chromeOptions.addProxy(chrome_options, ID)
+            # Chrome option 설정
+            chrome_options = webdriver.ChromeOptions()
+            chromeOptions.addOptimization(chrome_options, temp_profile_dir)
+            chromeOptions.addMobile(chrome_options)
+            hiPaiProxy = chromeOptions.addProxy(chrome_options, ID)
 
-        # Xvfb 설정
-        os.environ['DISPLAY'] = ':99'
+            # Xvfb 설정
+            os.environ['DISPLAY'] = ':99'
 
-        # ChromeDriver를 webdriver_manager를 사용하여 자동으로 설치 및 설정
-        service = Service(ChromeDriverManager().install())
+            # ChromeDriver를 webdriver_manager를 사용하여 자동으로 설치 및 설정
+            service = Service(ChromeDriverManager().install())
 
-        # WebDriver 인스턴스 생성    
-        driver = webdriver.Chrome(service=service, options=chrome_options)
+            # WebDriver 인스턴스 생성    
+            driver = webdriver.Chrome(service=service, options=chrome_options)
 
-        # Chrome 최적화
-        # chromeOptions.intercept(driver)
+            # Chrome 최적화
+            # chromeOptions.intercept(driver)
 
-        return driver, temp_profile_dir, hiPaiProxy
-    except Exception as e:
-        print(f"Error creating driver: {e}")
-        create_driver(profileNum, ID)
+            return driver, temp_profile_dir, hiPaiProxy
+        except:
+            time.sleep(3)
+            pass
--- hiPaiProxy.py
+++ hiPaiProxy.py
@@ -3,37 +3,39 @@ import time
 from datetime import datetime, timedelta
 
 def getProxyIp(ID):
-    try:
-        with open('hiPaiIp.txt', 'r') as file:
-            lines = file.readlines()
-    except Exception as e:
-        print(f"Error reading file: {e}")
-        time.sleep(3)
-        getProxyIp(ID)
+    for i in range(10):
+        try:
+            with open('hiPaiIp.txt', 'r') as file:
+                lines = file.readlines()
 
-    # 빈 줄 제거 및 각 줄에서 ','로 구분
-    lines = [line.strip() for line in lines if line.strip()]
+            # 빈 줄 제거 및 각 줄에서 ','로 구분
+            lines = [line.strip() for line in lines if line.strip()]
 
-    # lines 중 첫번째 줄 선택
-    selected_line = lines.pop(0)
+            # lines 중 첫번째 줄 선택
+            selected_line = lines.pop(0)
 
-    # 선택된 줄 hiPaiIp.txt 에서 제거
-    try:
-        with open('hiPaiIp.txt', 'w') as file:
-            for line in lines:
-                file.write(line + '\n')
-    except Exception as e:
-        print(f"Error writing file: {e}")
-        time.sleep(3)
-        getProxyIp(ID)
+            # 선택된 줄 hiPaiIp.txt 에서 제거
+            try:
+                with open('hiPaiIp.txt', 'w') as file:
+                    for line in lines:
+                        file.write(line + '\n')
+            except Exception as e:
+                print(f"Error writing file: {e}")
+                time.sleep(3)
+                getProxyIp(ID)
 
-    return selected_line
-
+            return selected_line
+        except Exception as e:
+            print(f"Error reading file: {e}")
+            time.sleep(3)
+            pass
+    
 def setProxyIp(hiPaiProxy):
-    try:
-        with open('hiPaiIp.txt', 'a') as file:
-            file.write(hiPaiProxy + '\n')
-    except Exception as e:
-        print(f"Error editing file: {e}")
-        time.sleep(3)
-        setProxyIp(hiPaiProxy)
+    for i in range(10):
+        try:
+            with open('hiPaiIp.txt', 'a') as file:
+                file.write(hiPaiProxy + '\n')
+        except Exception as e:
+            print(f"Error editing file: {e}")
+            time.sleep(3)
+            pass
